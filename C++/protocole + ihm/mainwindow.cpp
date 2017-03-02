ef405736c655b6c8899a0d6cb9190d2232f9a217
Jean-Loïc <jeanloicmugnier@hotmail.com>
2017-03-02 10:46:16 +0100

    new mainwindow.cpp with integration to the protocol

--- C++/protocole + ihm/mainwindow.cpp
+++ C++/protocole + ihm/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "controlstation.h"
+#include "rover.h"
 #include <QPushButton>
 #include <QFont>
 #include <QDesktopWidget>
@@ -15,6 +16,11 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    //setup rover and control station
+    Rover* r = new Rover(std::bitset<4>(0x0001));
+    this->cs = new ControlStation(r);
+
+
     // Setup base UI (see mainwindow.ui)
     ui->setupUi(this);
 
@@ -59,22 +65,29 @@ QString MainWindow::getDate() {
 }
 
 void MainWindow::clickForward() {
+    this->cs.move_rover(Direction::get_directions("FORWARD"), 1);
     ui->listInfos->addItem(getDate()+"Going forward");
 }
 
 void MainWindow::clickBackward() {
+    this->cs.move_rover(Direction::get_directions("BACKWARD"), 1);
     ui->listInfos->addItem(getDate()+"Going backward");
 }
 
 void MainWindow::clickLeft() {
+    this->cs.move_rover(Direction::get_directions("LEFT"), 1);
     ui->listInfos->addItem(getDate()+"Turning to the left");
+
 }
 
 void MainWindow::clickRight() {
+    this->cs.move_rover(Direction::get_directions("RIGHT"), 1);
     ui->listInfos->addItem(getDate()+"Turning to the right");
 }
 
 void MainWindow::clickStop() {
+
+    this->cs.move_rover(Direction::get_directions("STOP"), 1);
     ui->listInfos->addItem(getDate()+"Freeze all motor functions");
 }
 
@@ -82,6 +95,7 @@ void MainWindow::clickRec() {
     QString s = ui->comboRec->currentText();
     int recorded = checkRecorded(s);
     if(recorded<0) {
+    cs->start_recording(s.toUpper());
         ui->listInfos->addItem(getDate()+"Recording "+s);
         ui->listCurrentRec->addItem(s);
     }
@@ -94,6 +108,7 @@ void MainWindow::clickStopRec() {
     QString s = ui->comboRec->currentText();
     int recorded = checkRecorded(s);
     if(recorded>=0) {
+        cs->stop_recording(s.toUpper());
         ui->listInfos->addItem(getDate()+"Recording of "+s+" has stopped");
         QListWidgetItem *item = ui->listCurrentRec->item(recorded);
         delete item;
@@ -120,12 +135,15 @@ void MainWindow::securityLevel() {
     QString res = getDate()+"Security level set to ";
     QString s;
     if(ui->radioSecuLow->isChecked()) {
+        cs->change_security_rover(0);
         s = "Low";
     }
     else if(ui->radioSecuMed->isChecked()) {
+        cs->change_security_rover(1);
         s = "Medium";
     }
     else {
+        cs->change_security_rover(2);
         s = "High";
     }
     ui->listInfos->addItem(res+s);
